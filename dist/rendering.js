755fbbb526015fbc249b91958b42afd76e7c51e1
Трофимов Никита Владиславович <Trofimov.Ni.Vl@sberbank.ru>
2019-05-28 13:28:27 +0300

    fix hist

--- dist/rendering.js
+++ dist/rendering.js
@@ -1,9 +1,9 @@
 'use strict';
 
-System.register(['jquery', 'd3', 'chart.js'], function (_export, _context) {
+System.register(['jquery', 'd3'], function (_export, _context) {
     "use strict";
 
-    var $, d3, Chart;
+    var $, d3;
     function link(scope, elem, attrs, ctrl) {
         var panel = ctrl.panel;
 
@@ -14,84 +14,61 @@ System.register(['jquery', 'd3', 'chart.js'], function (_export, _context) {
         function render() {
             console.log(panel);
             if (panel.histData.length > 0 && panel.histData[0].items.length > 0) {
-                renderHist2();
+                renderHist();
             }
         }
 
-        function renderHist2() {
-            var container = elem.find('div.panel-content > .panel-height-helper');
-            var $tooltip = $('<canvas id="myChart" width="400" height="400"></canvas>');
-            container.appendChild($tooltip);
-            var ctx = document.getElementById('myChart').getContext('2d');
-            var myBarChart = new Chart(ctx, {
-                type: 'bar',
-                data: panel.histData[0].items,
-                options: {
-                    scales: {
-                        yAxes: [{
-                            ticks: {
-                                beginAtZero: true
-                            }
-                        }]
-                    }
-                }
-            });
-            myBarChart.render();
-        }
-
         function renderHist() {
-            var data = panel.histData[0];
-            var xData = data.items.map(function (d) {
-                return d.x;
-            });
-            var yData = data.items.map(function (d) {
+            // const xData = data.items.map(d => d.x);
+            var values = panel.histData[0].items.map(function (d) {
                 return d.y;
             });
-            console.log(xData);
-            console.log(yData);
+            var formatCount = d3.format(",.0f");
+            var color = "steelblue";
             var margin = { top: 10, right: 30, bottom: 30, left: 30 },
                 width = 960 - margin.left - margin.right,
                 height = 500 - margin.top - margin.bottom;
 
-            var x = d3.scaleTime().domain([xData[0], xData[xData.length - 1]]).rangeRound([0, width]);
+            var max = d3.max(values);
+            var min = d3.min(values);
+            var x = d3.scale.linear().domain([min, max]).range([0, width]);
+
+            var data = d3.layout.histogram().bins(x.ticks(20))(values);
+
             console.log(x);
+            console.log(data);
+            var yMax = d3.max(data, function (d) {
+                return d.length;
+            });
+            var yMin = d3.min(data, function (d) {
+                return d.length;
+            });
+            var colorScale = d3.scale.linear().domain([yMin, yMax]).range([d3.rgb(color).brighter(), d3.rgb(color).darker()]);
 
-            var y = d3.scaleLinear().range([height, 0]);
+            console.log(yMax);
+            console.log(yMin);
 
-            var histogram = d3.histogram().value(function (d) {
-                return d.format("DD-MM-YYYY");
-            }).domain(x.domain()).thresholds(x.ticks(d3.timeWeek));
+            var y = d3.scale.linear().domain([0, yMax]).range([height, 0]);
 
-            var svg = d3.select("div.panel-content > .panel-height-helper").append("svg").attr("width", width + margin.left + margin.right).attr("height", height + margin.top + margin.bottom).append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+            var xAxis = d3.svg.axis().scale(x).orient("bottom");
 
-            svg.append("g").attr("class", "axis axis--x").attr("transform", "translate(0," + height + ")").call(d3.axisBottom(x));
+            var svg = d3.select("body").append("svg").attr("width", width + margin.left + margin.right).attr("height", height + margin.top + margin.bottom).append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-            var bins = histogram(xData);
-
-            y.domain([0, d3.max(yData)]);
-
-            console.log(bins);
-
-            var bar = svg.selectAll(".bar").data(bins).enter().append("g").attr("class", "bar").attr("transform", function (d) {
-                return "translate(" + x(d.x0) + "," + y(d.length) + ")";
+            var bar = svg.selectAll(".bar").data(data).enter().append("g").attr("class", "bar").attr("transform", function (d) {
+                return "translate(" + x(d.x) + "," + y(d.y) + ")";
             });
 
-            bar.append("rect").attr("x", 1).attr("width", function (d) {
-                return x(d.x1) - x(d.x0) - 1;
-            }).attr("height", function (d) {
-                return height - y(d.length);
+            bar.append("rect").attr("x", 1).attr("width", x(data[0].dx) - x(0) - 1).attr("height", function (d) {
+                return height - y(d.y);
+            }).attr("fill", function (d) {
+                return colorScale(d.y);
             });
 
-            bar.append("text").attr("dy", ".75em").attr("y", 6).attr("x", function (d) {
-                return (x(d.x1) - x(d.x0)) / 2;
-            }).attr("text-anchor", "middle").text(function (d) {
-                return formatCount(d.length);
+            bar.append("text").attr("dy", ".75em").attr("y", -12).attr("x", (x(data[0].dx) - x(0)) / 2).attr("text-anchor", "middle").text(function (d) {
+                return formatCount(d.y);
             });
 
-            function type(d) {
-                d.date = parseDate(d.date);
-                return d;
-            }
+            svg.append("g").attr("class", "x axis").attr("transform", "translate(0," + height + ")").call(xAxis);
         }
     }
 
@@ -102,8 +79,6 @@ System.register(['jquery', 'd3', 'chart.js'], function (_export, _context) {
             $ = _jquery.default;
         }, function (_d) {
             d3 = _d;
-        }, function (_chartJs) {
-            Chart = _chartJs.default;
         }],
         execute: function () {}
     };
--- src/rendering.js
+++ src/rendering.js
@@ -1,6 +1,5 @@
 import $ from 'jquery';
 import * as d3 from "d3";
-import Chart from 'chart.js';
 
 export default function link(scope, elem, attrs, ctrl) {
     const panel = ctrl.panel;
@@ -12,93 +11,78 @@ export default function link(scope, elem, attrs, ctrl) {
     function render() {
         console.log(panel);
         if (panel.histData.length > 0 && panel.histData[0].items.length > 0) {
-            renderHist2();
+            renderHist();
         }
     }
 
-    function renderHist2() {
-        const container = elem.find('div.panel-content > .panel-height-helper');
-        var $tooltip = $('<canvas id="myChart" width="400" height="400"></canvas>');
-        container.appendChild($tooltip);
-        var ctx = document.getElementById('myChart').getContext('2d');
-        var myBarChart = new Chart(ctx, {
-            type: 'bar',
-            data: panel.histData[0].items,
-            options: {
-                scales: {
-                    yAxes: [{
-                        ticks: {
-                            beginAtZero: true
-                        }
-                    }]
-                }
-            }
-        });
-        myBarChart.render();
-    }
-
     function renderHist() {
-        const data = panel.histData[0];
-        const xData = data.items.map(d => d.x);
-        const yData = data.items.map(d => d.y);
-        console.log(xData);
-        console.log(yData);
+        // const xData = data.items.map(d => d.x);
+        const values = panel.histData[0].items.map(d => d.y);
+        var formatCount = d3.format(",.0f");
+        var color = "steelblue";
         var margin = {top: 10, right: 30, bottom: 30, left: 30},
             width = 960 - margin.left - margin.right,
             height = 500 - margin.top - margin.bottom;
 
-        var x = d3.scaleTime()
-            .domain([xData[0], xData[xData.length - 1]])
-            .rangeRound([0, width]);
-        console.log(x);
+        var max = d3.max(values);
+        var min = d3.min(values);
+        var x = d3.scale.linear()
+            .domain([min, max])
+            .range([0, width]);
 
-        var y = d3.scaleLinear()
+        var data = d3.layout.histogram()
+            .bins(x.ticks(20))
+            (values);
+
+        console.log(x);
+        console.log(data);
+        var yMax = d3.max(data, function(d){return d.length});
+        var yMin = d3.min(data, function(d){return d.length});
+        var colorScale = d3.scale.linear()
+            .domain([yMin, yMax])
+            .range([d3.rgb(color).brighter(), d3.rgb(color).darker()]);
+
+        console.log(yMax);
+        console.log(yMin);
+
+        var y = d3.scale.linear()
+            .domain([0, yMax])
             .range([height, 0]);
 
-        var histogram = d3.histogram()
-            .value(function(d) { return d.format("DD-MM-YYYY"); })
-            .domain(x.domain())
-            .thresholds(x.ticks(d3.timeWeek));
+        var xAxis = d3.svg.axis()
+            .scale(x)
+            .orient("bottom");
 
-        var svg = d3.select("div.panel-content > .panel-height-helper").append("svg")
+        var svg = d3.select("body").append("svg")
             .attr("width", width + margin.left + margin.right)
             .attr("height", height + margin.top + margin.bottom)
             .append("g")
             .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-        svg.append("g")
-            .attr("class", "axis axis--x")
-            .attr("transform", "translate(0," + height + ")")
-            .call(d3.axisBottom(x));
-
-        var bins = histogram(xData);
-
-        y.domain([0, d3.max(yData)]);
-
-        console.log(bins);
-
         var bar = svg.selectAll(".bar")
-            .data(bins)
+            .data(data)
             .enter().append("g")
             .attr("class", "bar")
-            .attr("transform", function(d) { return "translate(" + x(d.x0) + "," + y(d.length) + ")"; });
+            .attr("transform", function(d) { return "translate(" + x(d.x) + "," + y(d.y) + ")"; });
 
         bar.append("rect")
             .attr("x", 1)
-            .attr("width", function(d) { return x(d.x1) - x(d.x0) - 1; })
-            .attr("height", function(d) { return height - y(d.length); });
+            .attr("width", (x(data[0].dx) - x(0)) - 1)
+            .attr("height", function(d) { return height - y(d.y); })
+            .attr("fill", function(d) { return colorScale(d.y) });
 
         bar.append("text")
             .attr("dy", ".75em")
-            .attr("y", 6)
-            .attr("x", function(d) { return (x(d.x1) - x(d.x0)) / 2; })
+            .attr("y", -12)
+            .attr("x", (x(data[0].dx) - x(0)) / 2)
             .attr("text-anchor", "middle")
-            .text(function(d) { return formatCount(d.length); });
+            .text(function(d) { return formatCount(d.y); });
 
-        function type(d) {
-            d.date = parseDate(d.date);
-            return d;
-        }
+        svg.append("g")
+            .attr("class", "x axis")
+            .attr("transform", "translate(0," + height + ")")
+            .call(xAxis);
+
     }
 
 }
