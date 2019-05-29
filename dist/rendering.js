39e890ec9781935bc48591ac15c066e7629e32e8
Трофимов Никита Владиславович <Trofimov.Ni.Vl@sberbank.ru>
2019-05-29 11:36:42 +0300

    fix

--- dist/rendering.js
+++ dist/rendering.js
@@ -28,16 +28,31 @@ System.register(['jquery', 'd3'], function (_export, _context) {
                 width = 960 - margin.left - margin.right,
                 height = 500 - margin.top - margin.bottom;
 
+            var svg;
+
+            if (document.querySelectorAll("panel-plugin-test-clock-plugin.panel-height-helper ng-transclude.panel-height-helper svg").length === 0) {
+                svg = d3.select("panel-plugin-test-clock-plugin.panel-height-helper ng-transclude.panel-height-helper").append("svg");
+            } else {
+                svg = d3.select("panel-plugin-test-clock-plugin.panel-height-helper ng-transclude.panel-height-helper svg");
+            }
+
+            svg.attr("width", width + margin.left + margin.right).attr("height", height + margin.top + margin.bottom).append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
             var max = d3.max(values);
             var min = d3.min(values);
 
             var x = d3.scaleLinear().domain([min, max]).range([margin.left, width - margin.right]);
+            svg.append("g").attr("class", "x axis").attr("transform", "translate(0," + height + ")").call(d3.axisBottom(x));
 
-            var y = d3.scaleLinear().range([height - margin.bottom, margin.top]);
+            var y = d3.scaleLinear().range([height, 0]);
 
-            var data = d3.histogram().thresholds(x.ticks(values.length))(values);
+            var data = d3.histogram().domain(x.domain()).thresholds(x.ticks(values.length))(values);
 
-            console.log(x);
+            y.domain([0, d3.max(data, function (d) {
+                return d.length;
+            })]);
+            svg.append("g").call(d3.axisLeft(y));
+
             console.log(data);
 
             var yMax = d3.max(data, function (d) {
@@ -50,35 +65,32 @@ System.register(['jquery', 'd3'], function (_export, _context) {
             console.log(yMax);
             console.log(yMin);
 
-            var xAxis = d3.axisBottom(x);
+            // var bar = svg.selectAll(".bar")
+            //     .data(data)
+            //     .enter().append("g")
+            //     .attr("class", "bar")
+            //     .attr("transform", function(d) { return "translate(" + x(d.x0) + "," + y(d[0]) + ")"; });
+            //
+            // bar.append("rect")
+            //     .attr("x", 1)
+            //     .attr("width", function(d) { return x(d.x1) - x(d.x0) - 1; })
+            //     .attr("height", function(d) { return (height - y(d[0])) / 2; })
+            //     .attr("fill", '#0af');
+            //
+            // bar.append("text")
+            //     .attr("dy", ".75em")
+            //     .attr("y", -12)
+            //     .attr("x", function(d) { return (x(d.x1) - x(d.x0)) / 2; })
+            //     .attr("text-anchor", "middle")
+            //     .text(function(d) { return formatCount(d[0]); });
 
-            var svg;
-
-            if (document.querySelectorAll("panel-plugin-test-clock-plugin.panel-height-helper ng-transclude.panel-height-helper svg").length === 0) {
-                svg = d3.select("panel-plugin-test-clock-plugin.panel-height-helper ng-transclude.panel-height-helper").append("svg");
-            } else {
-                svg = d3.select("panel-plugin-test-clock-plugin.panel-height-helper ng-transclude.panel-height-helper svg");
-            }
-
-            svg.attr("width", width + margin.left + margin.right).attr("height", height + margin.top + margin.bottom).append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
-            var bar = svg.selectAll(".bar").data(data).enter().append("g").attr("class", "bar").attr("transform", function (d) {
-                return "translate(" + x(d.x0) + "," + y(d[0]) + ")";
-            });
-
-            bar.append("rect").attr("x", 1).attr("width", function (d) {
+            svg.selectAll("rect").data(data).enter().append("rect").attr("x", 1).attr("transform", function (d) {
+                return "translate(" + x(d.x0) + "," + y(d.length) + ")";
+            }).attr("width", function (d) {
                 return x(d.x1) - x(d.x0) - 1;
             }).attr("height", function (d) {
-                return height - y(d[0]);
-            }).attr("fill", '#0af');
-
-            bar.append("text").attr("dy", ".75em").attr("y", -12).attr("x", function (d) {
-                return (x(d.x1) - x(d.x0)) / 2;
-            }).attr("text-anchor", "middle").text(function (d) {
-                return formatCount(d[0]);
-            });
-
-            svg.append("g").attr("class", "x axis").attr("transform", "translate(0," + height + ")").call(xAxis);
+                return height - y(d.length);
+            }).style("fill", "#69b3a2");
         }
     }
 
--- src/rendering.js
+++ src/rendering.js
@@ -23,31 +23,6 @@ export default function link(scope, elem, attrs, ctrl) {
             width = 960 - margin.left - margin.right,
             height = 500 - margin.top - margin.bottom;
 
-        var max = d3.max(values);
-        var min = d3.min(values);
-
-        var x = d3.scaleLinear()
-            .domain([min, max])
-            .range([margin.left, width - margin.right]);
-
-        var y = d3.scaleLinear()
-            .range([height - margin.bottom, margin.top]);
-
-        var data = d3.histogram()
-            .thresholds(x.ticks(values.length))
-            (values);
-
-        console.log(x);
-        console.log(data);
-
-        var yMax = d3.max(data, function(d){return d[0]});
-        var yMin = d3.min(data, function(d){return d[0]});
-
-        console.log(yMax);
-        console.log(yMin);
-
-        var xAxis = d3.axisBottom(x);
-
         var svg;
 
         if (document.querySelectorAll("panel-plugin-test-clock-plugin.panel-height-helper ng-transclude.panel-height-helper svg").length === 0) {
@@ -62,30 +37,65 @@ export default function link(scope, elem, attrs, ctrl) {
             .append("g")
             .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-        var bar = svg.selectAll(".bar")
-            .data(data)
-            .enter().append("g")
-            .attr("class", "bar")
-            .attr("transform", function(d) { return "translate(" + x(d.x0) + "," + y(d[0]) + ")"; });
+        var max = d3.max(values);
+        var min = d3.min(values);
 
-        bar.append("rect")
-            .attr("x", 1)
-            .attr("width", function(d) { return x(d.x1) - x(d.x0) - 1; })
-            .attr("height", function(d) { return height - y(d[0]); })
-            .attr("fill", '#0af');
-
-        bar.append("text")
-            .attr("dy", ".75em")
-            .attr("y", -12)
-            .attr("x", function(d) { return (x(d.x1) - x(d.x0)) / 2; })
-            .attr("text-anchor", "middle")
-            .text(function(d) { return formatCount(d[0]); });
-
+        var x = d3.scaleLinear()
+            .domain([min, max])
+            .range([margin.left, width - margin.right]);
         svg.append("g")
             .attr("class", "x axis")
             .attr("transform", "translate(0," + height + ")")
-            .call(xAxis);
+            .call(d3.axisBottom(x));
 
+        var y = d3.scaleLinear()
+            .range([height, 0]);
+
+        var data = d3.histogram()
+            .domain(x.domain())
+            .thresholds(x.ticks(values.length))
+            (values);
+
+        y.domain([0, d3.max(data, function(d) { return d.length; })]);
+        svg.append("g")
+            .call(d3.axisLeft(y));
+
+        console.log(data);
+
+        var yMax = d3.max(data, function(d){return d[0]});
+        var yMin = d3.min(data, function(d){return d[0]});
+
+        console.log(yMax);
+        console.log(yMin);
+
+        // var bar = svg.selectAll(".bar")
+        //     .data(data)
+        //     .enter().append("g")
+        //     .attr("class", "bar")
+        //     .attr("transform", function(d) { return "translate(" + x(d.x0) + "," + y(d[0]) + ")"; });
+        //
+        // bar.append("rect")
+        //     .attr("x", 1)
+        //     .attr("width", function(d) { return x(d.x1) - x(d.x0) - 1; })
+        //     .attr("height", function(d) { return (height - y(d[0])) / 2; })
+        //     .attr("fill", '#0af');
+        //
+        // bar.append("text")
+        //     .attr("dy", ".75em")
+        //     .attr("y", -12)
+        //     .attr("x", function(d) { return (x(d.x1) - x(d.x0)) / 2; })
+        //     .attr("text-anchor", "middle")
+        //     .text(function(d) { return formatCount(d[0]); });
+
+        svg.selectAll("rect")
+            .data(data)
+            .enter()
+            .append("rect")
+            .attr("x", 1)
+            .attr("transform", function(d) { return "translate(" + x(d.x0) + "," + y(d.length) + ")"; })
+            .attr("width", function(d) { return x(d.x1) - x(d.x0) -1 ; })
+            .attr("height", function(d) { return height - y(d.length); })
+            .style("fill", "#69b3a2")
     }
 
 }
